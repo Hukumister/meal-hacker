6ebb4be6f114f750f6fdf9abb79c6c13761c1b6e
Zlata Zaltsman <zaltsman05@bk.ru>
2025-07-10 19:30:45 +0500

    Create the layout of the recipe screen page

--- src/app/recipe_card/page.tsx
+++ src/app/recipe_card/page.tsx
@@ -1,36 +1,16 @@
 'use client';
 import React from 'react';
-import {useEffect, useState } from 'react';
 import { Clock8, Flame, EggFried, ShoppingBag } from 'lucide-react';
-import { Recipe } from '@/types/recipe';
 
 
-const getRecipe = async (id: string): Promise<Recipe> => {
-  return {
-    id,
+export default function RecipeCard() {
+  const recipe = {
     title: 'Шоколадный Торт',
+    cookingTime: { active: 20, total: 40 },
     calories: 350,
     caloriesPer100: 100,
-    cookingTime: {
-      total: 40,
-      active: 20,
-      passive: 20,
-    },
-    nutritionPerServing: {
-      protein: 10,
-      fat: 15,
-      carbs: 40,
-    },
-    nutritionPer100: {
-      protein: 3,
-      fat: 5,
-      carbs: 20,
-    },
-    servings: 8,
-    weight: {
-      amount: 800,
-      unit: 'г',
-    },
+    nutritionPerServing: { protein: 10, fat: 15, carbs: 40 },
+    nutritionPer100: { protein: 3, fat: 5, carbs: 20 },
     ingredients: [
       { name: 'Мука', amount: 200, unit: 'г' },
       { name: 'Сахар', amount: 100, unit: 'г' },
@@ -52,46 +32,34 @@ const getRecipe = async (id: string): Promise<Recipe> => {
       'Укрась пудрой, орехами или ягодами.',
       'Подавай с чаем или кофе.',
     ],
-  };
 };
-
-export default function RecipePage() {
-  const [recipe, setRecipe] = useState<Recipe | null>(null);
-  useEffect(() => {
-    const fetchRecipe = async () => {
-      const recipeData = await getRecipe('1');
-      setRecipe(recipeData);
-    };
-    fetchRecipe();
-  }, []);
-
   return (
     <div className="max-w-6xl p-9 text-brown-800 font-sans">
-      <h1 className="text-3xl font-bold text-center mb-10">{recipe?.title}</h1>
+      <h1 className="text-3xl font-bold text-center mb-10">{recipe.title}</h1>
 
       <div className="border border-brown-800 rounded-xl p-4 bg-white text-brown-800 font-sans grid grid-cols-1 sm:grid-cols-3 gap-6 text-sm text-left my-6">
   
         <div className="flex flex-col items-center space-y-1">
           <Clock8 width={30} height={30}/>
           <div className="flex flex-col items-start">
-            <div><strong>Активное время:</strong> {recipe?.cookingTime.active} мин</div>
-            <div><strong>Полное время:</strong> {recipe?.cookingTime.total} мин</div>
+            <div><strong>Активное время:</strong> {recipe.cookingTime.active} мин</div>
+            <div><strong>Полное время:</strong> {recipe.cookingTime.total} мин</div>
           </div>
         </div>
 
         <div className="flex flex-col items-center space-y-1">
           <Flame width={30} height={30}/>
           <div className="flex flex-col items-start">
-            <div><strong>На порцию:</strong> {recipe?.calories} ккал</div>
-            <div><strong>На 100 г:</strong> {recipe?.caloriesPer100} ккал</div>
+            <div><strong>На порцию:</strong> {recipe.calories} ккал</div>
+            <div><strong>На 100 г:</strong> {recipe.caloriesPer100} ккал</div>
           </div>
         </div>
 
         <div className="flex flex-col items-center space-y-1">
           <EggFried width={30} height={30} />
           <div className="flex flex-col items-start">
-            <div><strong>На порцию:</strong> {recipe?.nutritionPerServing.protein}б {recipe?.nutritionPerServing.fat}ж {recipe?.nutritionPerServing.carbs}у</div>
-            <div><strong>На 100 г:</strong> {recipe?.nutritionPer100.protein}б {recipe?.nutritionPer100.fat}ж {recipe?.nutritionPer100.carbs}у</div>
+            <div><strong>На порцию:</strong> {recipe.nutritionPerServing.protein}б {recipe.nutritionPerServing.fat}ж {recipe.nutritionPerServing.carbs}у</div>
+            <div><strong>На 100 г:</strong> {recipe.nutritionPer100.protein}б {recipe.nutritionPer100.fat}ж {recipe.nutritionPer100.carbs}у</div>
           </div>
         </div>
       </div>
@@ -100,7 +68,7 @@ export default function RecipePage() {
         <div className="lg:w-1/3 max-w-sm rounded-lg bg-gray-100 p-6 text-left self-start">
           <h2 className="font-semibold text-xl mb-2">Ингредиенты:</h2>
           <ul className="list-disc pl-6 space-y-1">
-            {recipe?.ingredients.map((ing, idx) => (
+            {recipe.ingredients.map((ing, idx) => (
               <li key={idx}>
                 {ing.name} - {ing.amount} {ing.unit}
               </li>
@@ -113,7 +81,7 @@ export default function RecipePage() {
         <div className="w-full lg:flex-1 px-6 py-8 pt-2 bg-gradient-to-b from-white to-gray-100 rounded-lg">
           <h2 className="font-semibold text-xl mb-6 text-center">Шаги приготовления:</h2>
           <ol className="list-decimal list-inside space-y-5 text-gray-800">
-            {recipe?.instructions.map((step, idx) => (
+            {recipe.instructions.map((step, idx) => (
               <li key={idx}>{step}</li>
             ))}
           </ol>
